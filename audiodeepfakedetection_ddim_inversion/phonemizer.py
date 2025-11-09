49094702716f4e819ed197d371ac3fb79329c9a0
arminrebele <164416228+arminrebele@users.noreply.github.com>
2025-11-09 11:41:59 +0100

    feat(phonemizer): add function to build token vocabulary from dataset

--- audiodeepfakedetection_ddim_inversion/phonemizer.py
+++ audiodeepfakedetection_ddim_inversion/phonemizer.py
@@ -1,10 +1,12 @@
 import json
 import re
 import subprocess
+import sys
 from tqdm import tqdm
+from collections.abc import Iterable
 
 from audiodeepfakedetection_ddim_inversion.data.vctk import VCTKDataset
-from audiodeepfakedetection_ddim_inversion.paths import PHONEME_VOCAB_PATH
+from audiodeepfakedetection_ddim_inversion.paths import TOKEN_VOCABULARY_PATH
 
 PUNCTUATION_REGEX = r"[.,!?;:\"'()\[\]{}\-–—…]"
 SPLIT_REGEX = f'({PUNCTUATION_REGEX}| )'
@@ -12,9 +14,9 @@ SPECIAL_TOKENS = ['<pad>', '<unk>', '<bos>', '<eos>']
 
 def _check_espeak_installed():
     try:
-        result = subprocess.run(['espeak-ng', '--version'], capture_output=True, text=True, check=True, encoding='utf-8')
-        return True
-    except Exception:
+        subprocess.run(['espeak-ng', '--version'], capture_output=True, text=True, check=True, encoding='utf-8')
+    except (FileNotFoundError, subprocess.CalledProcessError):
+        print("ERROR: espeak-ng not installed!", file=sys.stderr)
         raise
 
 def _call_espeak_g2p(text: str, language: str) -> list[str]:
@@ -45,24 +47,45 @@ class PhonemeTokenizer:
 
     def __init__(self):
         self.language = "en-us"
-        self.vocabulary = self._load_vocabulary()
+        self.token_vocabulary = {} # token to id
+        self.id_to_token = {}
 
+        _check_espeak_installed()
+        self._load_token_vocabulary()
 
-    def _load_vocabulary(self):
-        pass
+    def _load_token_vocabulary(self):
+        try:
+            with open(TOKEN_VOCABULARY_PATH, 'r', encoding='utf-8') as f:
+                self.token_vocabulary = json.load(f)
+        except FileNotFoundError:
+            print(f"ERROR: {TOKEN_VOCABULARY_PATH} not found", file=sys.stderr)
+            raise
+
 
     def __call__(self, text: str) -> list[int]:
         pass
 
 
+def build_token_vocabulary(dataset: Iterable[dict], language: str = "en-us") -> dict[str, int]:
 
-def build_vocabulary():
-    pass
+    _check_espeak_installed()
+
+    tokens_from_text = set()
+    for sample in tqdm(dataset):
+        text = sample["text"]
+        phonemized_text = _text_to_phonemes(text, language)
+        tokens_from_text.update(phonemized_text)
+
+    tokens = SPECIAL_TOKENS + sorted(tokens_from_text)
+
+    token_vocabulary = {token: idx for idx, token in enumerate(tokens)}
+
+    with open(TOKEN_VOCABULARY_PATH, "w", encoding="utf-8") as f:
+        json.dump(token_vocabulary, f, ensure_ascii=False, indent=2)
 
 
 
 if __name__ == "__main__":
-    #build_vocabulary()
     language = "en-us"
     text = "123 Hello, world! This is a test."
     phonemized_text = _text_to_phonemes(text, language)
