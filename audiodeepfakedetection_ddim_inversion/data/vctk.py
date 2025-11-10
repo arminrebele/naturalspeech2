4061d774cfab8d2e87491ed96a6ce4bfdf0daa2f
arminrebele <164416228+arminrebele@users.noreply.github.com>
2025-11-10 11:23:26 +0100

    feat(vctk-dataset): add phoneme tokenization step during preprocessing

--- audiodeepfakedetection_ddim_inversion/data/vctk.py
+++ audiodeepfakedetection_ddim_inversion/data/vctk.py
@@ -1,11 +1,18 @@
 from torch.utils.data import Dataset
-from datasets import load_dataset, load_from_disk, Audio
+from torch.nn.utils.rnn import pad_sequence
+from datasets import load_dataset, load_from_disk, Audio, Sequence, Value
 
 from audiodeepfakedetection_ddim_inversion.paths import VCTK_DIR, VCTK_PROCESSED_DIR
+from audiodeepfakedetection_ddim_inversion.data.phoneme_tokenizer import PhonemeTokenizer
+
+def tokenize_text(texts, phoneme_tokenizer):
+    # texts is list of strings (batched=True + input_columns=["text"])
+    return {"token_ids": [phoneme_tokenizer(text) for text in texts]}
 
 class VCTKDataset(Dataset):
-    def __init__(self, sampling_rate=24000):
+    def __init__(self, phoneme_tokenizer: PhonemeTokenizer = None, sampling_rate=24000):
         self.sampling_rate = sampling_rate
+        self.phoneme_tokenizer = phoneme_tokenizer or PhonemeTokenizer()
         self.dataset = self._process_dataset()
 
     def _process_dataset(self):
@@ -13,9 +20,24 @@ class VCTKDataset(Dataset):
             return load_from_disk(str(VCTK_PROCESSED_DIR))
         except Exception:
             VCTK_DIR.mkdir(parents=True, exist_ok=True)
+
             dataset = load_dataset("sanchit-gandhi/vctk", split="train", cache_dir=str(VCTK_DIR))
-            dataset = dataset.filter(lambda sample: "_mic2" in sample, input_columns=["file"])
+            dataset = dataset.filter(lambda file: "_mic2" in file, input_columns=["file"])
             dataset = dataset.cast_column("audio", Audio(sampling_rate=self.sampling_rate))
+
+            new_features = dataset.features.copy()
+            new_features["token_ids"] = Sequence(Value("int32"))
+            dataset = dataset.map(
+                tokenize_text,
+                fn_kwargs={"phoneme_tokenizer": self.phoneme_tokenizer},
+                input_columns=["text"],
+                batched=True,
+                batch_size=256,
+                num_proc=4,
+                features=new_features,
+                desc="Tokenizing transcripts",
+            )
+
             VCTK_PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
             dataset.save_to_disk(str(VCTK_PROCESSED_DIR))
             return dataset
@@ -27,8 +49,9 @@ class VCTKDataset(Dataset):
         item = self.dataset[idx]
         return {
             "audio": item["audio"]["array"],
+            "text": item["text"],
+            "token_ids": item["token_ids"],
             "sampling_rate": item["audio"]["sampling_rate"],
-            "transcript": item["text"],
             "speaker_id": item["speaker_id"],
             "age": int(item["age"]), 
             "gender": item["gender"],
@@ -36,7 +59,8 @@ class VCTKDataset(Dataset):
             "region": item["region"],
         }
 
-
+def vctk_collate_fn(batch):
+    pass
 
 if __name__ == "__main__":
     
