b9867a38a98445d4277483c9fbf97ed228213f76
arminrebele <164416228+arminrebele@users.noreply.github.com>
2025-11-15 23:52:35 +0100

    feat(aligner): implement ForwardSumLoss class

--- audiodeepfakedetection_ddim_inversion/aligner.py
+++ audiodeepfakedetection_ddim_inversion/aligner.py
@@ -33,33 +33,40 @@ class Aligner(nn.Module):
         
         alignment_soft, alignment_logits = self.aligner_net(audio_encodings, phoneme_encodings, phoneme_tokens_mask)  # [B, 1, F, P]
 
+        # combine masks [B,1,F] & [B,1,P] -> [B,F,P]
+        frame_mask_2d = frame_mask.squeeze(1).bool()   # [B,F]
+        phoneme_tokens_mask_2d = phoneme_tokens_mask.squeeze(1).bool()   # [B,P]
+        attn_mask = frame_mask_2d.unsqueeze(2) & phoneme_tokens_mask_2d.unsqueeze(1)  # [B,F,P]
+
+        alignment_soft_2d = alignment_soft.squeeze(1)           # [B,F,P]
+        B, F, P = alignment_soft_2d.shape
+        alignment_logprobs = torch.log(alignment_soft_2d + 1e-9) # [B,F,P]
+          
+        prior_logprobs = compute_beta_binomial_prior(  # [B,F,P]
+            frame_lengths,
+            phoneme_tokens_lengths, 
+            frames_max=F,
+            phoneme_tokens_max=P, 
+            w=1.0 
+        )
+
+        alignment_logits_2d = alignment_logits.squeeze(1)        # [B, F, P]
+        alignment_logits_with_prior = alignment_logits_2d + prior_logprobs # [B, F, P]
+
+        alignment_logprobs_for_viterbi = alignment_logprobs + prior_logprobs # [B,F,P]
+
         with torch.no_grad():
-            # combine masks [B,1,F] & [B,1,P] -> [B,F,P]
-            frame_mask_2d = frame_mask.squeeze(1).bool()   # [B,F]
-            phoneme_tokens_mask_2d = phoneme_tokens_mask.squeeze(1).bool()   # [B,P]
-            attn_mask = frame_mask_2d.unsqueeze(2) & phoneme_tokens_mask_2d.unsqueeze(1)  # [B,F,P]
-
-            alignment_soft_2d = alignment_soft.squeeze(1)           # [B,F,P]
-            B, F, P = alignment_soft_2d.shape
-            alignment_logprobs = torch.log(alignment_soft_2d + 1e-9) # [B,F,P]
-            prior_logprobs = compute_beta_binomial_prior(
-                frame_lengths,
-                phoneme_tokens_lengths, 
-                frames_max=F,
-                phoneme_tokens_max=P, 
-                w=1.0 
-            )
-
-            alignment_logprobs_for_viterbi = alignment_logprobs + prior_logprobs # [B,F,P]
             alignment_hard = maximum_path(alignment_logprobs_for_viterbi, attn_mask, frame_lengths, phoneme_tokens_lengths)  # [B,F,P]
             durations = alignment_hard.sum(dim=1).int()   # [B,P]
 
-            return {
-                "durations": durations, 
-                "alignment_hard": alignment_hard,
-                "alignment_soft": alignment_soft,
-                "alignment_logprobs": alignment_logprobs
-            }
+        return {
+            "durations": durations,                     # [B,P]
+            "alignment_hard": alignment_hard,           # [B, F, P]
+            "alignment_soft": alignment_soft,           # [B, 1, F, P]
+            "alignment_logprobs": alignment_logprobs,   # [B, F, P]
+            "attn_mask": attn_mask,                     # [B, F, P]
+            "alignment_logits_with_prior": alignment_logits_with_prior,     # [B, F, P]
+        }
 
 
 
@@ -228,6 +235,64 @@ def compute_beta_binomial_prior(
 
 
 
+class ForwardSumLoss(nn.Module):
+    """
+
+    """
+    def __init__(self, blank_logprob: float = -1e4):
+        super().__init__()
+        self.blank_logprob = blank_logprob
+        self.ctc_loss = nn.CTCLoss(blank=0, reduction="mean", zero_infinity=True)
+        self.log_softmax = nn.LogSoftmax(dim=-1)
+
+    def forward(
+        self,
+        alignment_logits: torch.Tensor,   # [B, F, P] # must be alignment_logits_with_prior
+        frame_lengths: torch.Tensor,       # [B]
+        phoneme_tokens_lengths: torch.Tensor,  # [B]
+    ) -> torch.Tensor:
+        
+        device = alignment_logits.device
+        B, F, P = alignment_logits.shape
+
+        alignment_logits = alignment_logits.unsqueeze(1)  # [B, 1, F, P]
+
+        alignment_logits_padded = torch.nn.functional.pad(   #  [B, 1, F, P+1]
+            alignment_logits,
+            pad=(1, 0, 0, 0, 0, 0, 0, 0), 
+            value=self.blank_logprob,
+        )
+
+        losses = []
+
+        for b in range(B):
+            T = int(frame_lengths[b].item())            # Frames
+            N = int(phoneme_tokens_lengths[b].item())   # Tokens
+
+            curr = alignment_logits_padded[b:b+1, :, :T, : N + 1]    # [1, 1, T, P+1]
+            curr = curr[0]                                  # [1, T, P+1]
+
+            # ctc format [T, B=1, C]
+            curr = curr.permute(1, 0, 2).contiguous()       # [T, 1, P+1]
+
+            log_probs = self.log_softmax(curr)              # [T, 1, P+1]
+
+            target = torch.arange(1, N + 1, dtype=torch.long, device=device).unsqueeze(0)  # [1, N]
+
+            input_lengths = torch.tensor([T], dtype=torch.long, device=device)
+            target_lengths = torch.tensor([N], dtype=torch.long, device=device)
+
+            loss_b = self.ctc_loss(log_probs, target, input_lengths, target_lengths)
+            losses.append(loss_b)
+
+        return torch.stack(losses).mean()
+
+
+
+
+class BinLoss(nn.Module):
+    def forward():
+        pass
 
 if __name__ == "__main__":
     pass
