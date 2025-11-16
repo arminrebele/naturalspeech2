1d4318d2b88f324068d7a9325dc529908e4dd4a0
arminrebele <164416228+arminrebele@users.noreply.github.com>
2025-11-16 14:36:47 +0100

    feat(training): add initial structure for training function

---
+++ audiodeepfakedetection_ddim_inversion/naturalspeech2_training.py
@@ -0,0 +1,25 @@
+import torch
+from torch.utils.data import DataLoader
+
+
+from audiodeepfakedetection_ddim_inversion.data.vctk import VCTKDataset, vctk_collate_fn
+from audiodeepfakedetection_ddim_inversion.naturalspeech2_model import NaturalSpeech2Model
+
+
+def train():
+    device = "mps" if torch.backends.mps.is_available() else ("cuda" if torch.cuda.is_available() else "cpu")
+    
+    config = {} # TODO: define model config
+    
+    dataset = VCTKDataset()
+    loader = DataLoader(dataset, batch_size=4, shuffle=False, collate_fn=vctk_collate_fn)
+
+    model = NaturalSpeech2Model(config).to(device)
+
+
+    
+
+
+if __name__ == "__main__":
+    # train()
+    pass
