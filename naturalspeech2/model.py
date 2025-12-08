ed462af393e52bc480220483ee22aa073f37ebab
arminrebele <164416228+arminrebele@users.noreply.github.com>
2025-12-08 13:09:48 +0100

    docs(model): add type annotations and dimension comments to forward method parameters

--- naturalspeech2/model.py
+++ naturalspeech2/model.py
@@ -73,18 +73,21 @@ class NaturalSpeech2Model(nn.Module):
 
     def forward(
         self,
-        audio: torch.Tensor,
-        audio_mask: torch.Tensor, 
-        audio_lengths: torch.Tensor,
+        audio: torch.Tensor,                  # [B, T]    | float
+        audio_mask: torch.Tensor,             # [B, 1, T] | True/False
+        audio_lengths: torch.Tensor,          # [B]       | int
 
-        phoneme_tokens: torch.Tensor,
-        phoneme_tokens_mask: torch.Tensor,
-        phoneme_tokens_lengths: torch.Tensor,
+        phoneme_tokens: torch.Tensor,         # [B, P]    | int
+        phoneme_tokens_mask: torch.Tensor,    # [B, 1, P] | True/False
+        phoneme_tokens_lengths: torch.Tensor, # [B]       | int
     ):
 
         audio_encodings, frame_mask, frame_lengths = self.log_mel_spectrogram_generator(audio, audio_lengths)
-        
-        phoneme_encodings = self.phoneme_encoder(
+        # audio_encodings: [B, n_mels, F]
+        # frame_mask: [B, 1, F]
+        # frame_lengths: [B]
+
+        phoneme_encodings = self.phoneme_encoder(   # [B, dim_hidden, P]
             phoneme_tokens,
             phoneme_tokens_mask,
             phoneme_tokens_lengths
