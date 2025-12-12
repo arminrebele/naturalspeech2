560aa178f3d4a12886ae5000dcc9646f5f2535fe
arminrebele <164416228+arminrebele@users.noreply.github.com>
2025-12-13 00:10:25 +0100

    fix(transformer-encoder-layer): add comment to evaluate dropout application in forward pass of TransformerEncoderLayer

--- naturalspeech2/transformer_encoder_layer.py
+++ naturalspeech2/transformer_encoder_layer.py
@@ -45,11 +45,11 @@ class TransformerEncoderLayer(nn.Module):
         qmask = mask.transpose(1, 2).to(x.dtype)  # [B, P, 1]
 
         attn_out = self.multi_head_attention(self.norm1(x), mask)
-        x = x + self.dropout(attn_out)
+        x = x + self.dropout(attn_out) # EVALUATE: maybe additional dropout here to aggressive, lower value / drop completly
         x = x * qmask # padding token vector to zero
 
         ffn_out = self.conv1d_feed_forward(self.norm2(x), mask)
-        x = x + self.dropout(ffn_out)
+        x = x + self.dropout(ffn_out) # EVALUATE: maybe additional dropout here to aggressive, lower value / drop completly
         x = x * qmask # padding token vector to zero
 
         return x
