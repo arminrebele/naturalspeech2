cf6e49588200fa8502763254ec5d1a69f3b449de
arminrebele <164416228+arminrebele@users.noreply.github.com>
2025-12-14 19:21:46 +0100

    fix(utils): add create_mask_from_lengths function for mask generation from sequence lengths

--- naturalspeech2/utils/utils.py
+++ naturalspeech2/utils/utils.py
@@ -1,5 +1,14 @@
 import torch
 from torch.nn.utils.rnn import pad_sequence
 
+from einops import rearrange
 
+def create_mask_from_lengths(
+        lengths: torch.Tensor,   # [B]
+        max_len: int
+):
+    device = lengths.device
 
+    seq_range = torch.arange(max_len, device=device)
+    mask = rearrange(seq_range, 't -> 1 1 t') < rearrange(lengths, 'b -> b 1 1')
+    return mask  # [B, 1, T]
