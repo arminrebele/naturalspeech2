576ca6572488493a343a508c55828e73261dfa2f
arminrebele <164416228+arminrebele@users.noreply.github.com>
2025-12-20 18:01:37 +0100

    rename transformer_encoder_layer.py to modules.py

--- naturalspeech2/phoneme_encoder.py
+++ naturalspeech2/phoneme_encoder.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 from einops import rearrange
 
-from naturalspeech2.transformer_encoder_layer import TransformerEncoderLayer, RMSNorm
+from naturalspeech2.modules import TransformerEncoderLayer, RMSNorm
 
 class PhonemeEncoder(nn.Module):
     def __init__(
