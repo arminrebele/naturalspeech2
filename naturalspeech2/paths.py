625485c0a02028dff7631938f7a4808d7b1b0a8d
LionPoellath <lion.poellath@gmx.de>
2026-03-12 16:00:29 +0100

    refactor((params): restructuring of hyperparamter-configs and project paths

--- naturalspeech2/paths.py
+++ naturalspeech2/paths.py
@@ -4,11 +4,8 @@ PACKAGE_ROOT = Path(__file__).resolve().parent    # NATURALSPEECH2/naturalspeech
 PROJECT_ROOT = PACKAGE_ROOT.parent                # NATURALSPEECH2
 
 DATA_DIR = PROJECT_ROOT / "data"
-VCTK_DIR = DATA_DIR / "vctk"
-VCTK_PROCESSED_DIR = DATA_DIR / "vctk_processed"
 
 MODELS_DIR = PROJECT_ROOT / "models"
 ENCODEC_24KHZ_DIR = MODELS_DIR / "encodec_24khz"
 
 CONFIG_DIR = PACKAGE_ROOT / "config"
-TOKEN_VOCABULARY_PATH = DATA_DIR / "token_vocabulary.json"
